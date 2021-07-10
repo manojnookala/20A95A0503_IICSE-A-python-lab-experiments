7626253c4ecf663c4f68bbd18335f2ab517e4748
manoj <56528802+manojnookala@users.noreply.github.com>
2021-07-10 03:38:30 -0600

    Create listoperation.py

---
+++ listoperation.py
@@ -0,0 +1,23 @@
+#listoperation
+def list_sum(l):
+    s=0
+    for i in l:
+        s+=i
+    return s
+def list_sort(l):
+    l.sort()
+    return l
+def string_list_conversions(s):
+    li = list(s)
+    return li
+
+#list access from another module
+from listoperation import *
+print(list_sum([1,2,3,4,5]))
+print(list_sort([9,2,3,4,10]))
+print(string_list_conversions("hello world"))
+
+OUTPUT:
+15
+[2, 3, 4, 9, 10]
+['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
