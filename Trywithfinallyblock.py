eb9955d75798b93ce92b597f4257a5f68d0ad549
manoj <56528802+manojnookala@users.noreply.github.com>
2021-07-26 03:30:13 -0600

    Create Trywithfinallyblock.py

---
+++ Trywithfinallyblock.py
@@ -0,0 +1,14 @@
+try:
+  print("python")
+except:
+  print("python is in theory subject")
+else:
+  print("python is program subject")
+finally:
+    print("Python is printing")
+
+OUTPUT:
+python
+python is program subject
+Python is printing
+>>> 
