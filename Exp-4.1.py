50f545b4317a8256755afa79da27ab7171707ac7
manoj <56528802+manojnookala@users.noreply.github.com>
2021-08-09 14:58:10 +0530

    Create Exp-4.1.py

---
+++ Exp-4.1.py
@@ -0,0 +1,15 @@
+PROGRAM:
+import math as m
+sum=0
+n=2000000
+for n in range(2,n+1):
+    prime=True
+    for i in range(2,int(m.sqrt(n)+1)):
+        if n%i==0:
+            prime=False
+            break
+        if prime:
+            sum=sum+n
+print("sum={}".format(sum))
+OUTPUT:
+  sum=207851932781152
