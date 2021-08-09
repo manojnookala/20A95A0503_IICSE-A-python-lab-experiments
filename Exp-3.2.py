aac89e3479dd7bf9e98eec2fd7ba291a70f5a1f5
manoj <56528802+manojnookala@users.noreply.github.com>
2021-08-09 14:42:08 +0530

    Update Exp-3.2.py

--- Exp-3.2.py
+++ Exp-3.2.py
@@ -2,17 +2,28 @@
 3.2)
 Implement a python script using for loop that loops over a sequence.
 '''
-sequence = [1,2,3,4,5,6,7,8]
-for i in sequence :
-  print(i)
+st=int(input('Enter starting limit'))
+en=int(input('Enter ending limit'))
+for i in range(st,en+1):
+    print(i)
 '''
-Output
-1
-2
-3
-4
-5
-6
-7
-8
-'''
+OUTPUT:
+Enter starting limit15
+Enter ending limit30
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+
